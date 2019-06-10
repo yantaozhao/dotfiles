87bb3a341b37e0f7d4c75346ee68c6c134b85f2c
YantaoZhao <zhytsh15@126.com>
2019-06-10 18:53:19 +0800

    interactive install

--- ubuntu/ubuntu_install_pkgs.sh
+++ ubuntu/ubuntu_install_pkgs.sh
@@ -14,19 +14,26 @@ while getopts ":f" opt; do
     esac
 done
 
+read -p "Run `sudo apt update`? [y/N]" yn
+if [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]; then
+    sudo apt update
+fi
 
-sudo apt install wget
+# sudo apt install wget
 # sudo apt install curl
 sudo apt install aria2
 
 ## git:
 sudo apt install git
 sudo apt install gitk
-if [[ ! -f $HOME/.gitalias.txt ]] || [[ $FORCEMODE ]]; then
-    echo 'configuring git'
-    aria2c https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt -o ~/.gitalias.txt
-    sleep 1
-    git config --global include.path ~/.gitalias.txt
+if [[ ! -f $HOME/.gitalias.txt ]]; then
+    read -p "Configure git using gitalias? [y/N]" yn
+    if [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]; then
+        echo 'configuring git'
+        aria2c https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt -o ~/.gitalias.txt
+        sleep 1
+        git config --global include.path ~/.gitalias.txt
+    fi
 fi
 
 sudo apt install bash-completion
@@ -55,45 +62,58 @@ fi
 ## zsh:
 sudo apt install zsh
 ZSHRC="$HOME/.zshrc"
-if [[ ! -d $HOME/.oh-my-zsh ]] || [[ $FORCEMODE ]]; then
-    echo 'configuring oh-my-zsh'
-    # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    aria2c https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o omz_install.sh
-    sh omz_install.sh
-    sleep 1
-    if [ -f $ZSHRC ]; then
-        cp -iv $ZSHRC $ZSHRC.orig
-        sed -i 's/^[^#]*ZSH_THEME=.*/ZSH_THEME="af-magic"/' $ZSHRC
-        sed -i 's/^plugins=(git)$/plugins=(git colored-man-pages)/' $ZSHRC
-    else
-        echo "Warning: $ZSHRC not found!"
+if [[ ! -d $HOME/.oh-my-zsh ]]; then
+    read -p "Configure zsh using oh-my-zsh? [y/N]" yn
+    if [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]; then
+        echo 'configuring oh-my-zsh'
+        # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        aria2c https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o omz_install.sh
+        sh omz_install.sh
+        sleep 1
+        if [ -f $ZSHRC ]; then
+            cp -iv $ZSHRC $ZSHRC.orig
+            sed -i 's/^[^#]*ZSH_THEME=.*/ZSH_THEME="af-magic"/' $ZSHRC
+            sed -i 's/^plugins=(git)$/plugins=(git colored-man-pages)/' $ZSHRC
+        else
+            echo "Warning: $ZSHRC not found!"
+        fi
     fi
 fi
 
-if [[ ! `command -v rg` ]] || [[ $FORCEMODE ]]; then
-    echo 'installing ripgrep'
-    aria2c https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb
-    sudo dpkg -i ripgrep_11.0.1_amd64.deb
-    if [ -f $ZSHRC ]; then
-        echo -e "if command -v rg &> /dev/null; then alias rg=\"rg -u\"; fi" >> $ZSHRC
+if [[ ! `command -v rg` ]]; then
+    read -p "Install ripgrep? [y/N]" yn
+    if [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]; then
+        echo 'installing ripgrep'
+        aria2c https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb
+        sudo dpkg -i ripgrep_11.0.1_amd64.deb
+        if [ -f $ZSHRC ]; then
+            echo -e "if command -v rg &> /dev/null; then alias rg=\"rg -u\"; fi" >> $ZSHRC
+        else
+            echo "Warning: $ZSHRC not found!"
+        fi
     fi
 fi
 
-if [[ ! `command -v fd` ]] || [[ $FORCEMODE ]]; then
-    echo 'installing fd'
-    aria2c https://github.com/sharkdp/fd/releases/download/v7.3.0/fd_7.3.0_amd64.deb
-    sudo dpkg -i fd_7.3.0_amd64.deb
-    if [ -f $ZSHRC ]; then
-        echo -e "if command -v fd &> /dev/null; then alias fd=\"fd -s -HI\"; fi" >> $ZSHRC
+if [[ ! `command -v fd` ]]; then
+    read -p "Install fd? [y/N]" yn
+    if [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]; then
+        echo 'installing fd'
+        aria2c https://github.com/sharkdp/fd/releases/download/v7.3.0/fd_7.3.0_amd64.deb
+        sudo dpkg -i fd_7.3.0_amd64.deb
+        if [ -f $ZSHRC ]; then
+            echo -e "if command -v fd &> /dev/null; then alias fd=\"fd -s -HI\"; fi" >> $ZSHRC
+        else
+            echo "Warning: $ZSHRC not found!"
+        fi
     fi
 fi
 popd
 
 
 ## vscodium:
-read -p "Do you want to install vscodium? [y/N]" yn
-if [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]; then
-    if [[ ! `command -v vscodium` ]] || [[ $FORCEMODE ]]; then
+if [[ ! `command -v vscodium` ]]; then
+    read -p "Install vscodium? [y/N]" yn
+    if [[ "$yn" = "y" ]] || [[ "$yn" = "Y" ]]; then
         echo 'installing vscodium'
         wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
         echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
